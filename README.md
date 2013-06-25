25b8e215bfd8facda5a3baaf2b5f2b6e66dc3135
Eric Chio <eric@bhotels.me>
2013-06-26 01:47:02 +0800

    First usage of blending!!! Real improvements.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 Dataset is provided by UCI Machine Learning Laborary : http://archive.ics.uci.edu/ml/datasets/Wine+Quality
 
+- blending.py - A blend of RandomForest, ExtraTress and GradientBoostingClassifier. Accuracy is normally at 97% or 100%!
 - decision_tree.py - Basic decision tree usage in PYthon via Sci-kit learn.
 - gradient_boosting_tree.py - Basic gradient boosting tree in Python via Sci-kit learn.
 - extra_trees.py - Basic extra trees classifier usage in Python via Sci-kit learn.
@@ -10,4 +11,3 @@ Dataset is provided by UCI Machine Learning Laborary : http://archive.ics.uci.ed
 - random_forest.r - Basic random forest usage in R, this yields 100% accuracy.
 - random_forest_with_variable_training_set.r - This investigates the effect of accuracy by providing different number of training set per class to train.
 - random_forest_with_variable_training_set_and_variable_trees.r - This investigates the effect of accuracy by providing different number of training set per class to train and variable number of trees in random forest.
-
---
+++ blending.py
@@ -0,0 +1,56 @@
+﻿"""
+A blend of RandomForest, ExtraTress and GradientBoostingClassifier. Accuracy is normally at 97% or 100%!
+"""
+import csv
+import random
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from collections import Counter
+
+def accuracy(Y_predict, Y_test):
+    equal = 0
+    for i in xrange(len(Y_predict)):
+        if Y_predict[i] == Y_test[i]:
+            equal += 1
+
+    print 'Accuracy = %s' % (float(equal)/len(Y_predict))
+
+data = [ i for i in csv.reader(file('wine.data', 'rb')) ]
+random.shuffle(data)
+
+X = [ i[1:] for i in data ]
+Y = [ i[0] for i in data ]
+
+train_cutoff = len(data) * 3/4
+
+X_train = X[:train_cutoff]
+Y_train = Y[:train_cutoff]
+X_test = X[train_cutoff:]
+Y_test = Y[train_cutoff:]
+
+classifiers = [
+    RandomForestClassifier(n_estimators=10, criterion='gini'),
+    RandomForestClassifier(n_estimators=10, criterion='entropy'),
+    ExtraTreesClassifier(n_estimators=10, criterion='gini'),
+    ExtraTreesClassifier(n_estimators=10, criterion='entropy'),
+    GradientBoostingClassifier(n_estimators=10),
+]
+
+Y_predict = [ [] for i in xrange(len(classifiers)) ]
+
+for i, classifier in enumerate(classifiers):
+    classifier.fit(X_train, Y_train)
+    Y_predict[i] = classifier.predict(X_test)
+
+Y_mean = []
+for i in xrange(len(Y_predict[0])):
+    counter = Counter([Y_predict[classifier_id][i] for classifier_id in xrange(len(classifiers))])
+    vote = counter.most_common(1)[0][0] # if a tie occurs, the smaller predicted number wins
+    Y_mean.append(vote)
+    
+    # Uncomment this code to see the voting and the tie-breaking here
+    # for classifier_id in xrange(len(classifiers)):
+    #     print '%s %s %s %s %s => %s' % (Y_predict[0][i], Y_predict[1][i], Y_predict[2][i], Y_predict[3][i], Y_predict[4][i], vote)
+    
+accuracy(Y_mean, Y_test)
