ee8301578743da4a3777490d84ce9dc67ac3b505
Eric Chio <eric@bhotels.me>
2013-06-25 23:26:20 +0800

    Introducing ExtraTreesClassifier and GradientBoostingTreeClassifier.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 Dataset is provided by UCI Machine Learning Laborary : http://archive.ics.uci.edu/ml/datasets/Wine+Quality
 
+- gradient_boosting_tree.py - Basic gradient boosting tree in Python via Sci-kit learn.
+- extra_trees.py - Basic extra trees classifier usage in Python via Sci-kit learn.
 - random_forest.py - Basic random forest usage in Python via Sci-kit learn, this does not yield 100% accuracy but mostly 95%.
 - random_forest.r - Basic random forest usage in R, this yields 100% accuracy.
 - random_forest_with_variable_training_set.r - This investigates the effect of accuracy by providing different number of training set per class to train.
---
+++ gradient_boosting_tree.py
@@ -0,0 +1,33 @@
+﻿"""
+This uses GradientBoostingClassifier from Scikit-learn. Accuracy in general is 91% .
+"""
+import csv
+import random
+from sklearn.ensemble import GradientBoostingClassifier
+
+data = [ i for i in csv.reader(file('wine.data', 'rb')) ]
+random.shuffle(data)
+
+X = [ i[1:] for i in data ]
+Y = [ i[0] for i in data ]
+
+train_cutoff = len(data) * 3/4
+
+X_train = X[:train_cutoff]
+Y_train = Y[:train_cutoff]
+X_test = X[train_cutoff:]
+Y_test = Y[train_cutoff:]
+
+classifier = GradientBoostingClassifier(n_estimators=10)
+classifier = classifier.fit(X_train, Y_train)
+
+Y_predict = classifier.predict(X_test)
+
+equal = 0
+for i in xrange(len(Y_predict)):
+    if Y_predict[i] == Y_test[i]:
+        equal += 1
+
+print 'Accuracy = %s' % (float(equal)/len(Y_predict))
+
+
